3f35f0ecc30638e10e1879643e4240f407b8e866
Andrew Shoell <mrlunchbox777@gmail.com>
2021-06-10 21:20:25 -0600

    removing some unneeded powershell aliases

--- Alias/powershell/profile.git.ps1
+++
@@ -1,8 +0,0 @@
-function start-gcob([String]$branchname, [String]$remote = "origin") {git checkout -b $branchname; git push -u $remote $branchname;}
-Set-Alias gcob "start-gcob"
-function start-gbdr([String]$branchname, [String]$remote = "origin") {git push $remote --delete $branchname;}
-Set-Alias gbdr "start-gbdr"
-function start-gsmua { git submodule update --recursive --remote }
-Set-Alias gsmua "start-gsmua"
-
-Import-Module posh-git
