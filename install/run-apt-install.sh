afefdadef806290ed74e18f7aa05f8448f568ea2
Andrew Shoell <mrlunchbox777@gmail.com>
2021-06-14 16:24:37 -0600

    updating the apt install script

--- install/run-apt-install.sh
+++ install/run-apt-install.sh
@@ -1,7 +1,11 @@
 # run apt install function
 run-apt-install-basic-setup () {
   if [ "$2" == true ]; then
-    sudo apt-get install $1 -y
+    if [ -z $(which $1) ]; then
+      sudo apt-get install $1 -y
+    else
+      echo "$1 already installed, skipping."
+    fi
   else
     echo "Skipping install for $1..."
   fi
