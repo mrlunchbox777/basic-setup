b65fedff733621dce2ae859693e73615c783207b
Andrew Shoell <mrlunchbox777@gmail.com>
2021-06-16 16:53:46 -0600

    updating the gitconfig run

--- install/bash-installs/run-gitconfig-update.sh
+++ install/bash-installs/run-gitconfig-update.sh
@@ -1,7 +1,7 @@
 # run gitconfig update function
 run-gitconfig-update-basic-setup () {
-  if [ -z "$(grep 'path = .*gitconfig' ~/.gitconfig)" ]; then
-    echo -e "\n[include]\n  path = \"$DIR/gitconfig\"" >> ~/.gitconfig
+  if [ -z "$(grep 'path = .*basic-setup-gitconfig' ~/.gitconfig)" ]; then
+    echo -e "\n[include]\n  path = \"$DIR/basic-setup-gitconfig\"" >> ~/.gitconfig
   else
     echo "Skipping update for .gitconfig..."
   fi
