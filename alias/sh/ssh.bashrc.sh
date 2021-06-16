b5fc771f0746fe5dd1e19d879728d77d73527910
Andrew Shoell <mrlunchbox777@gmail.com>
2021-06-16 16:03:06 -0600

    updating the ssh.bashrc.sh to use source instead of .

--- alias/sh/ssh.bashrc.sh
+++ alias/sh/ssh.bashrc.sh
@@ -7,14 +7,14 @@ function start_ssh_agent {
   /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
   echo "succeeded"
   chmod 600 "${SSH_ENV}"
-  . "${SSH_ENV}" > /dev/null
+  source "${SSH_ENV}" > /dev/null
   /usr/bin/ssh-add
 }
 
 # Source SSH settings, if applicable
 
 if [ -f "${SSH_ENV}"]; then
-  . "${SSH_ENV}" > /dev/null
+  source "${SSH_ENV}" > /dev/null
   #ps ${SSH_AGENT_PID} doesn't work under cygwin
   ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
     start_ssh_agent;
