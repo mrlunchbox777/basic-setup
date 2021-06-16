960bb6fcbbb9a4bb2cb3283f4f29f033e7cbe507
Andrew Shoell <mrlunchbox777@gmail.com>
2021-06-15 18:46:43 -0600

    adding logging to the update commands

--- install/bash-installs/run-batcat-update.sh
+++ install/bash-installs/run-batcat-update.sh
@@ -3,6 +3,10 @@ run-batcat-update-basic-setup () {
   if [ -z $(which bat) ]; then
     if [ -z $(which batcat) ]; then
       ln -s "$(which batcat)" "$(dirname $(which batcat))/bat"
+    else
+      echo "Skipping update for batcat, batcat doesn't exist..."
     fi
+  else
+    echo "Skipping update for batcat, already exits..."
   fi
 }
--- install/bash-installs/run-gitconfig-update.sh
+++ install/bash-installs/run-gitconfig-update.sh
@@ -2,5 +2,7 @@
 run-gitconfig-update-basic-setup () {
   if [ -z "$(grep 'path = .*gitconfig' ~/.gitconfig)" ]; then
     echo -e "\n[include]\n  path = \"$DIR/gitconfig\"" >> ~/.gitconfig
+  else
+    echo "Skipping update for .gitconfig..."
   fi
 }
