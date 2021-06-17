a55b252dec3cef3df87cecb9a168cf42f8b76e3e
Andrew Shoell <mrlunchbox777@gmail.com>
2021-06-16 20:50:39 -0600

    fixing the apt install to use dpkg instead of which

--- install/bash-installs/run-apt-install.sh
+++ install/bash-installs/run-apt-install.sh
@@ -2,7 +2,7 @@
 run-apt-install-basic-setup () {
   check_for_run_variable_name="should_install_$(echo $1 | sed -r 's/-/_/g')"
   if [ "${!check_for_run_variable_name}" == true ]; then
-    if [ -z $(which $1) ]; then
+    if [ -z "$(dpkg -l | grep $1)" ]; then
       sudo apt-get install $1 -y
     else
       echo "$1 already installed, skipping."
