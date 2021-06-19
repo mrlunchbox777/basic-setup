b1306877cb6d8017a82910d4a56bfd50751edf82
Andrew Shoell <mrlunchbox777@gmail.com>
2021-06-19 16:43:18 -0600

    removing the unusable code in the basic-setup.sh and adding a new shared script for finding source and dir of a script

--- basic-setup.sh
+++ basic-setup.sh
@@ -15,9 +15,6 @@ if [ ! -d basic-setup ]; then
   git clone https://github.com/mrlunchbox777/basic-setup
 fi
 
-cd basic-setup
-for f in $(ls ./shared-scripts/sh/); do source ./shared-scripts/sh/$f; done
-
 bash ./install/init.sh | tee basic-setup-sh-output.log
 
 ## end of basic setup
---
+++ shared-scripts/sh/run-get-source-and-dir.sh
@@ -0,0 +1,33 @@
+# run-get-source-and-dir function
+run-get-source-and-dir() {
+  local run_get_source_and_dir_help_string=""
+  run_get_source_and_dir_help_string+="error - no source passed in\\n"
+  run_get_source_and_dir_help_string+="pass in source using the following\\n"
+  run_get_source_and_dir_help_string+="\\n"
+  run_get_source_and_dir_help_string+="* sh - source=\"\$0\"\\n"
+  run_get_source_and_dir_help_string+="* bash - source=\"\${BASH_SOURCE[0]}\"\\n"
+  run_get_source_and_dir_help_string+="* zsh - source=\"\${(%):-%x}\"\\n"
+  run_get_source_and_dir_help_string+="\\n"
+  run_get_source_and_dir_help_string+="output - \"source\" \"dir\"\\n"
+  run_get_source_and_dir_help_string+="to use run - eval \$(run-get-source-and-dir \"\$source\")\\n"
+  run_get_source_and_dir_help_string+="\\n"
+  run_get_source_and_dir_help_string+="after the eval statement\\n"
+  run_get_source_and_dir_help_string+="* \$source will be set to source resolving symlinks\\n"
+  run_get_source_and_dir_help_string+="* \$dir will be set to the parent dir of \$source\\n"
+
+  local source="$1"
+  local dir=""
+
+  if [ -z "$source" ]; then
+    echo -e "$run_get_source_and_dir_help_string" >&2
+    [[ $- == *i* ]] && echo '' || exit 1
+  fi
+
+  while [ -h "$source" ]; do # resolve $source until the file is no longer a symlink
+    dir="$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )"
+    source="$(readlink "$source")"
+    [[ $source != /* ]] && source="$dir/$source" # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  done
+  dir="$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )"
+  echo "source=\"$source\"; dir=\"$dir\""
+}
