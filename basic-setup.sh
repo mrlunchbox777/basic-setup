506e14330c8d73f51bc83718ef327331281f5ff1
Andrew Shoell <mrlunchbox777@gmail.com>
2021-06-18 22:08:36 -0600

    trying to get better spacing on the basic-setup

--- basic-setup.sh
+++ basic-setup.sh
@@ -23,9 +23,9 @@ bash ./install/init.sh | tee basic-setup-sh-output.log
 ## end of basic setup
 echo \
 "\n\n\n\n\n" \
-"Finished Basic Setup\n" \
-"  Check -\n" \
-"    ~/src/tools/basic-setup/basic-setup-sh-output.log\n" \
-"  It will have logs and outputs on everything installed.\n"
+"Finished Basic Setup\n\n" \
+"  Check -\n\n" \
+"    ~/src/tools/basic-setup/basic-setup-sh-output.log\n\n" \
+"  It will have logs and outputs on everything installed.\n\n"
 
 cd "$currentDir"
