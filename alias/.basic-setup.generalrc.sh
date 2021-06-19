28097bdb76ab95746d445ed4b9f2d6db064abd44
Andrew Shoell <mrlunchbox777@gmail.com>
2021-06-19 17:10:35 -0600

    updating the rc's to use the shared scripts

--- alias/.basic-setup.generalrc.sh
+++ alias/.basic-setup.generalrc.sh
@@ -1,12 +1,7 @@
 # Run all the shell scripts in the sh folder
 
-# Pulled from https://stackoverflow.com/questions/7665/how-to-resolve-symbolic-links-in-a-shell-script
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+for f in $(ls ../shared-scripts/sh/); do source ../shared-scripts/sh/$f; done
+source="${BASH_SOURCE[0]}"
+eval $(run-get-source-and-dir "$source")
 
-for f in $(ls $DIR/sh/); do source $DIR/sh/$f; done
+for f in $(ls $dir/sh/); do source $dir/sh/$f; done
