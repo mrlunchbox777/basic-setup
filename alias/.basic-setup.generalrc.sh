7a61b8b48bc7eb1bf12cb018bd5956a1e92a0806
Andrew Shoell <mrlunchbox777@gmail.com>
2021-06-19 17:14:00 -0600

    changing source to sh for compatibility

--- alias/.basic-setup.generalrc.sh
+++ alias/.basic-setup.generalrc.sh
@@ -1,7 +1,7 @@
 # Run all the shell scripts in the sh folder
 
-for f in $(ls ../shared-scripts/sh/); do source ../shared-scripts/sh/$f; done
+for f in $(ls ../shared-scripts/sh/); do . ../shared-scripts/sh/$f; done
 source="${BASH_SOURCE[0]}"
 eval $(run-get-source-and-dir "$source")
 
-for f in $(ls $dir/sh/); do source $dir/sh/$f; done
+for f in $(ls $dir/sh/); do . $dir/sh/$f; done
