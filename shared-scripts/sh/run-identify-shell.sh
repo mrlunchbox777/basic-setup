caf674e092b20a1d89f85744fc9ac9efef3832df
Andrew Shoell <mrlunchbox777@gmail.com>
2021-06-19 15:42:09 -0600

    fixing typo

--- shared-scripts/sh/run-identify-shell.sh
+++ shared-scripts/sh/run-identify-shell.sh
@@ -1,5 +1,5 @@
 # run identify shell function
 run-identify-shell-basic-setup () {
   local shell=$(ps -o args= -p "$$" | awk '{print $1}' | awk -F '/' '{print $NF}')
-  ehco "$shell"
+  echo "$shell"
 }
