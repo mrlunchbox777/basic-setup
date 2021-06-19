a309059e0d058332e10d5bb9beba2a25a5df7765
Andrew Shoell <mrlunchbox777@gmail.com>
2021-06-19 17:37:23 -0600

    fixing the generalrc so that it's the only one that is needed

--- alias/.basic-setup.bashrc
+++
@@ -1,7 +0,0 @@
-# Run all the shell scripts in the bash folder
-
-for f in $(ls ../shared-scripts/sh/); do source ../shared-scripts/sh/$f; done
-source="${BASH_SOURCE[0]}"
-eval $(run-get-source-and-dir "$source")
-
-for f in $(ls $dir/bash/); do source $dir/bash/$f; done
--- alias/.basic-setup.generalrc.sh
+++ alias/.basic-setup.generalrc.sh
@@ -1,7 +1,29 @@
 # Run all the shell scripts in the sh folder
 
 for f in $(ls ../shared-scripts/sh/); do . ../shared-scripts/sh/$f; done
-source="${BASH_SOURCE[0]}"
+source=""
+
+case run-identify-shell in
+  sh)
+    echo "using sh aliases"
+    source="$0"
+    extra_folder=""
+    ;;
+  bash)
+    echo "using bash aliases"
+    source="${BASH_SOURCE[0]}"
+    extra_folder="bash"
+    ;;
+  zsh)
+    echo "using zsh aliases"
+    source="${(%):-%x}"
+    extra_folder="zsh"
+    ;;
+esac
+
 eval $(run-get-source-and-dir "$source")
 
 for f in $(ls $dir/sh/); do . $dir/sh/$f; done
+if [ ! -z "$extrafolder" ]
+  for f in $(ls $dir/$extra_folder/); do source $dir/$extra_folder/$f; done
+fi
