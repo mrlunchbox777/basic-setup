97140c821e00b9dcb6f69c39b91cc5dadb461a74
Andrew Shoell <mrlunchbox777@gmail.com>
2021-06-20 16:49:46 -0600

    updating function name to match regular standards

--- install/sh-installs/run-update-alias.sh
+++ install/sh-installs/run-update-alias.sh
@@ -1,5 +1,5 @@
 # run update alias function
-run-single-update-alias() {
+run-single-update-alias-basic-setup() {
   if [ ! -f "$HOME/$1" ]; then
     touch "$HOME/$1"
   fi
@@ -14,8 +14,8 @@ run-single-update-alias() {
 run-update-alias-basic-setup () {
   local target_dir="$(readlink -f "$dir/../alias/basic-setup.generalrc.sh")"
 
-  run-single-update-alias ".profile" "$target_dir"
-  run-single-update-alias ".zprofile" "$target_dir"
-  run-single-update-alias ".bashrc" "$target_dir"
-  run-single-update-alias ".zshrc" "$target_dir"
+  run-single-update-alias-basic-setup ".profile" "$target_dir"
+  run-single-update-alias-basic-setup ".zprofile" "$target_dir"
+  run-single-update-alias-basic-setup ".bashrc" "$target_dir"
+  run-single-update-alias-basic-setup ".zshrc" "$target_dir"
 }
