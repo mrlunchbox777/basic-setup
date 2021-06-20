9acc65ca218017d04c7506833d0836d9c42e63f4
Andrew Shoell <mrlunchbox777@gmail.com>
2021-06-19 19:19:10 -0600

    adding aliases for shared scripts

---
+++ alias/sh/shared-scripts.sh
@@ -0,0 +1,11 @@
+# Aliases for shared scripts
+
+alias full-update='run-full-update-basic-setup'
+
+alias get-sandd='run-get-source-and-dir'
+
+alias id-shell='run-identify-shell-basic-setup'
+
+alias rsm='run-send-message'
+
+alias gsm='run-update-gitsubmodule-basic-setup'
