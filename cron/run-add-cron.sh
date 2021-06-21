ae84caa9cf231c84c0d9c486510a30dbf409ccd1
Andrew Shoell <mrlunchbox777@gmail.com>
2021-06-20 20:10:09 -0600

    adding a todo for cron tasks

---
+++ cron/run-add-cron.sh
@@ -0,0 +1,8 @@
+#run-add-cron.sh
+
+# TODO: Add a format for cronjobs
+# (crontab -l 2>/dev/null; echo "*/5 * * * * /path/to/job -with args") | crontab -
+
+# examples
+# https://stackoverflow.com/questions/4880290/how-do-i-create-a-crontab-through-a-script
+# https://stackoverflow.com/questions/878600/how-to-create-a-cron-job-using-bash-automatically-without-the-interactive-editor
