5b467a4c8889923ed5edc0a29786f82f62089681
Andrew Shoell <mrlunchbox777@gmail.com>
2021-06-23 16:04:54 -0600

    making sure that we refresh the environment variables after installing

--- install/powershell-installs/Install-ChocoPackage.ps1
+++ install/powershell-installs/Install-ChocoPackage.ps1
@@ -3,6 +3,8 @@ function Install-ChocoPackageBasicSetup($package) {
   $check_for_run_variable_name="should_install_$($1 -replace '-','_')"
   if ( $true -eq [System.Environment]::GetEnvironmentVariable("$check_for_run_variable_name") ) {
     Write-Output "choco install $package -y"
+
+    refresh-env
   }
 }
 
