89cb85eb6c0a41db3329a987a70e35d3e0a59b7f
Andrew Shoell <mrlunchbox777@gmail.com>
2021-06-22 23:47:37 -0600

    adding variables to set up a windows environment

--- install/powershell-installs/Install-ChocoPackage.ps1
+++ install/powershell-installs/Install-ChocoPackage.ps1
@@ -1,12 +1,20 @@
 # Install-ChocoPackageBasicSetup
 function Install-ChocoPackageBasicSetup($package) {
-  Write-Output "choco install $package -y"
+  $check_for_run_variable_name="should_install_$($1 -replace '-','_')"
+  if ( $true -eq [System.Environment]::GetEnvironmentVariable("$check_for_run_variable_name") ) {
+    Write-Output "choco install $package -y"
+  }
 }
 
 function Install-ManyChocoPackageBasicSetup() {
   $packages=""
   foreach ($package in $args) {
-    $packages+="$package"
+    $check_for_run_variable_name="should_install_$($1 -replace '-','_')"
+    if ( $true -eq [System.Environment]::GetEnvironmentVariable("$check_for_run_variable_name") ) {
+      $packages+="$package"
+    }
   }
-  Install-ChocoPackageBasicSetup "$packages"
+  Write-Output "choco install $package -y"
+
+  refresh-env
 }
