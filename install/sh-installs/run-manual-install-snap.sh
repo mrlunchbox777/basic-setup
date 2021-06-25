1fd85dbd3b66b1c86e96c9b544a58223d3345a83
Andrew Shoell <mrlunchbox777@gmail.com>
2021-06-24 18:57:18 -0600

    fixing the snap installs because of string interpolation

--- install/sh-installs/run-manual-install-snap.sh
+++ install/sh-installs/run-manual-install-snap.sh
@@ -16,17 +16,21 @@ run-manual-install-snap-many-basic-setup () {
   local snap_install_string=""
   for run_manual_install_snap_many_basic_setup_f in "$@"
   do
-    local check_for_run_variable_name="should_install_$(echo $run_manual_install_snap_many_basic_setup_f | sed -r 's/-/_/g')"
+    local current_run_manual_install_snap_many_basic_setup_f="$(echo $run_manual_install_snap_many_basic_setup_f | xargs echo)"
+    local check_for_run_variable_name="should_install_$(echo $current_run_manual_install_snap_many_basic_setup_f | sed -r 's/-/_/g')"
     if [ "${!check_for_run_variable_name}" == true ]; then
-      if [ -z "$(dpkg -l | grep $run_manual_install_snap_many_basic_setup_f)" ]; then
-        snap_install_string+="$run_manual_install_snap_many_basic_setup_f "
+      if [ -z "$(which $current_run_manual_install_snap_many_basic_setup_f)" ]; then
+        snap_install_string+="$current_run_manual_install_snap_many_basic_setup_f "
       else
-        echo "$run_manual_install_snap_many_basic_setup_f already installed, skipping."
+        echo "$current_run_manual_install_snap_many_basic_setup_f already installed, skipping."
       fi
     else
-      echo "Skipping install for $run_manual_install_snap_many_basic_setup_f..."
+      echo "Skipping install for $current_run_manual_install_snap_many_basic_setup_f..."
     fi
   done
 
-  sudo snap install $snap_install_string
+  snap_install_string="$(echo $snap_install_string | xargs echo)"
+  if [ ! -z "$snap_install_string" ]; then
+    sudo snap install "$snap_install_string" 
+  fi
 }
