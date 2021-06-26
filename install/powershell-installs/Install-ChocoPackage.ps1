d99378fcd61a3279c0880bf28379b974a9d8bb81
Andrew Shoell <mrlunchbox777@gmail.com>
2021-06-26 17:32:20 -0600

    fixing some whitespace

--- install/powershell-installs/Install-ChocoPackage.ps1
+++ install/powershell-installs/Install-ChocoPackage.ps1
@@ -1,7 +1,7 @@
 # Install-ChocoPackageBasicSetup
 function Install-ChocoPackageBasicSetup($package) {
   $checkForRunVariableName="ShouldInstall_$($1 -replace '-','_')"
-  if ( $true -eq [System.Environment]::GetEnvironmentVariable("$checkForRunVariableName") ) {
+  if ($true -eq [System.Environment]::GetEnvironmentVariable("$checkForRunVariableName")) {
     Write-Output "choco install $package -y"
 
     refresh-env
@@ -12,7 +12,7 @@ function Install-ManyChocoPackageBasicSetup() {
   $packages=""
   foreach ($package in $args) {
     $checkForRunVariableName="ShouldInstall_$($1 -replace '-','_')"
-    if ( $true -eq [System.Environment]::GetEnvironmentVariable("$checkForRunVariableName") ) {
+    if ($true -eq [System.Environment]::GetEnvironmentVariable("$checkForRunVariableName")) {
       $packages+="$package "
     }
   }
