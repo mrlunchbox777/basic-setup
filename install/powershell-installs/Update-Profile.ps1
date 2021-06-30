d9d8473f27394bffc601afa86379813c041340a2
Andrew Shoell <mrlunchbox777@gmail.com>
2021-06-30 17:24:27 -0600

    Updating the script for setting up the Profile.ps1 that should work better now

--- install/powershell-installs/Update-Profile.ps1
+++ install/powershell-installs/Update-Profile.ps1
@@ -1,7 +1,7 @@
 # Update-ProfileBasicSetup
 function Update-ProfileBasicSetup() {
-  $profileSet = "$(Select-String ".*basic-setup\.profile\.ps1" "$profile")"
-  if (($true -eq "$env:ShouldUpdateBasicSetupProfile") -and ($true -eq "$profileSet")) {
+  $profileSet = "$(Select-String "\..*basic-setup\.profile\.ps1" "$profile")"
+  if (($true -eq "$env:ShouldUpdateBasicSetupProfile") -and ([System.String]::IsNullOrWhiteSpace("$profileSet"))) {
     $target_dir="$env:DIR/../alias/basic-setup.profile.ps1"
     Write-Output "" >> "$profile"
     Write-Output ". \"$target_dir\"" >> "$profile"
