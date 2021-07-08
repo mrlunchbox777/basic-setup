514cc969f46d9b3986fdbfaee3b0ad4add4bda7a
Andrew Shoell <mrlunchbox777@gmail.com>
2021-07-07 22:29:03 -0600

    Updating the cheatsheet

--- alias/sh/cheatsheet.sh
+++ alias/sh/cheatsheet.sh
@@ -1,10 +1,14 @@
 cheatsheet() {
   echo "
-    Cheatsheet
+    ***Cheatsheet***
+    
+    * basic command line tutorial https://ubuntu.com/tutorials/command-line-for-beginners#1-overview
 
+    **Command Info**
     tldr - get simple info on command
     man - get detailed info on command
 
+    **Backend Commands**
     lsof - find process that has file
     wget|curl - make network request
     tail - display the last part of the file
@@ -14,6 +18,7 @@ cheatsheet() {
     ssh - secure shell, for remoting
     kill - terminate a process/job
 
+    **Text manipulation**
     awk - find per line
     sed - edit text with regex
     grep - match regex
@@ -29,17 +34,21 @@ cheatsheet() {
     fgrep - egrep in file
     wc - count lines (-l), words (-w), characters (-c), in file
 
+    **Compilation**
     gcc - compile C & C++
     make - run Makefiles
 
+    **Process Manipulation**
     ps - info on running processes
     top|htop|atop - real-time info on running processes
 
+    **Performance and Telemetry**
     nmon - benchmarking, tuning, admin
     iostat - stats for devices and partitions
     sar - stats on Linux subsystems
     vmstat - hardware telemetry reporting
 
+    **Networking**
     nmap - network mapping
     tcpdump - dump network traffic
     ping - send ICMP request
@@ -51,15 +60,19 @@ cheatsheet() {
     iptables - configuration of tables, chains, and rules
     netstat - display network info
 
+    **Debugging**
     strace|dtrace|systemtap - trace processes
     uname - info about current system
     df - filesystem usage
     history - commandline history
 
+    **Personal Recommendations**
     watch - repeat command on interval
-    tmux - terminal multiplexer
+    tmux|screen - terminal multiplexer
     <command> & - run command as background job
     jobs - list current jobs
+    script - record shell session to file
+    vim - text editor (https://vim-adventures.com/)
 
     https://github.com/kamranahmedse/developer-roadmap
   "
