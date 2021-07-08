fe95222227bb2b36864f6c138b3a586384f8a670
Andrew Shoell <mrlunchbox777@gmail.com>
2021-07-07 22:15:48 -0600

    adding a basic cheatsheet

---
+++ alias/sh/cheatsheet.sh
@@ -0,0 +1,66 @@
+cheatsheet() {
+  echo "
+    Cheatsheet
+
+    tldr - get simple info on command
+    man - get detailed info on command
+
+    lsof - find process that has file
+    wget|curl - make network request
+    tail - display the last part of the file
+    head - interactive 
+    less - read first part of content
+    find - find in directory structure
+    ssh - secure shell, for remoting
+    kill - terminate a process/job
+
+    awk - find per line
+    sed - edit text with regex
+    grep - match regex
+    sort - sort thing per line
+    uniq - uniq string per line
+    cat|bat - prints file
+    cut - cut string per line
+    echo - prints text
+    fmt - formats text to 75 char
+    tr - text & replace
+    nl - number lines
+    egrep - extended grep, using ?, +, {, }, |, (, )
+    fgrep - egrep in file
+    wc - count lines (-l), words (-w), characters (-c), in file
+
+    gcc - compile C & C++
+    make - run Makefiles
+
+    ps - info on running processes
+    top|htop|atop - real-time info on running processes
+
+    nmon - benchmarking, tuning, admin
+    iostat - stats for devices and partitions
+    sar - stats on Linux subsystems
+    vmstat - hardware telemetry reporting
+
+    nmap - network mapping
+    tcpdump - dump network traffic
+    ping - send ICMP request
+    mtr - ping/traceroute
+    traceroute - prints route to host
+    dig - dns lookup
+    airmon-ng - monitor wireless devices
+    airodump-ng - packet capturing
+    iptables - configuration of tables, chains, and rules
+    netstat - display network info
+
+    strace|dtrace|systemtap - trace processes
+    uname - info about current system
+    df - filesystem usage
+    history - commandline history
+
+    watch - repeat command on interval
+    tmux - terminal multiplexer
+    <command> & - run command as background job
+    jobs - list current jobs
+
+    https://github.com/kamranahmedse/developer-roadmap
+  "
+}
