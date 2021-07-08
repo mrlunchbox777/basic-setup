2948568594565f94ed0f474d6958dda2e81eb868
Andrew Shoell <mrlunchbox777@gmail.com>
2021-07-07 23:07:18 -0600

    moving networking to it's own alias

--- alias/sh/primary.sh
+++ alias/sh/primary.sh
@@ -66,25 +66,3 @@ find-files-ignore() {
 count-lines-ignore() {
   find-files-ignore "$@" | xargs wc -l
 }
-
-my-public-ip() {
-  dig +short myip.opendns.com @resolver1.opendns.com
-}
-
-my-default-network-device() {
-  ip route show default | awk '/default/ {print}' | tr " " "\n" | awk '/([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)|(([a-f0-9:]+:+)+[a-f0-9]+)/ {getline;getline;print}'
-}
-
-my-mac() {
-  my-default-network-device | xargs -I{} cat /sys/class/net/{}/address | sed -E ':a;N;$!ba;s/\r{0,1}\n/,/g'
-}
-
-my-default-route() {
-  local device=$(my-default-network-device)
-  ip addr show | awk "/$device/ {print}"
-}
-
-my-local-ip() {
-  local device=$(my-default-network-device)
-  ip addr show | awk "/$device/ {print}" | tr " " "\n" | awk '/([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)|(([a-f0-9:]+:+)+[a-f0-9]+)/ {print;exit}'
-}
