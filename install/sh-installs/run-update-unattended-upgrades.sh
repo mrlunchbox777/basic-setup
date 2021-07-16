18c85097e7d62f6cbf105650fde77df8cab23162
Andrew Shoell <mrlunchbox777@gmail.com>
2021-07-16 15:34:28 -0600

    finishing the unattended updates configuration

--- install/sh-installs/run-update-unattended-upgrades.sh
+++ install/sh-installs/run-update-unattended-upgrades.sh
@@ -1,9 +1,8 @@
 # run update unattended-upgrades function
 run-update-unattended-upgrades-basic-setup () {
   # https://help.ubuntu.com/community/AutomaticSecurityUpdates
-  echo "update unattended upgrades"
-  # TODO find a way to only update the config if it hasn't already been updated
-  # if [ -z $(which unattended-upgrades) ]; then
-  #   sudo dpkg-reconfigure --priority=low unattended-upgrades
-  # fi
+  if [ ! -z $(which unattended-upgrades) ]; then
+    echo "update unattended upgrades"
+    sudo dpkg-reconfigure --priority=low unattended-upgrades
+  fi
 }
