67d214e459db6a56899304bd95100836d5a4458e
Andrew Shoell <mrlunchbox777@gmail.com>
2021-07-17 09:43:16 -0600

    updating the cheatsheet a little bit

--- alias/sh/cheatsheet.sh
+++ alias/sh/cheatsheet.sh
@@ -19,8 +19,8 @@ cheatsheet() {
     lsof - find process that has file
     wget|curl - make network request
     tail - display the last part of the file
-    head - interactive 
-    less - read first part of content
+    head - read first part of content
+    less - interactive read content
     find - find in directory structure
     ssh - secure shell, for remoting
     kill - terminate a process/job
