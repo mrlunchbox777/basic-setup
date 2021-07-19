81bfc13a049302497134e97790c5e645a773ceea
Andrew Shoell <mrlunchbox777@gmail.com>
2021-07-18 22:38:31 -0600

    changing the choco run to actually run instead of debugging

--- install/powershell-installs/Install-ChocoPackage.ps1
+++ install/powershell-installs/Install-ChocoPackage.ps1
@@ -3,8 +3,7 @@ function Install-ChocoPackageBasicSetup($package) {
   $checkForRunVariableName="ShouldInstall_$($package -replace '-','_')"
   $checkForRunVariableName="ShouldInstall_$($checkForRunVariableName -replace '.',':')"
   if ($true -eq [System.Environment]::GetEnvironmentVariable("$checkForRunVariableName")) {
-    Write-Output "choco install $package -y"
-
+    choco install $package -y
     refreshenv
   }
 }
@@ -16,19 +15,15 @@ function Install-ManyChocoPackageBasicSetup() {
     [String[]]
     $packagesArray
   )
+
   $packages=""
   foreach ($package in $packagesArray) {
-    Write-Output "current package - $package"
     $checkForRunVariableName="ShouldInstall_$($package -replace '-','_')"
     if ($true -eq [System.Environment]::GetEnvironmentVariable("$checkForRunVariableName")) {
-      Write-Output "adding package - $package"
       $packages+="$package "
-    } else {
-      $currentRunVal=[System.Environment]::GetEnvironmentVariable("$checkForRunVariableName")
-      Write-Output "skipping package - $package - $checkForRunVariableName - $currentRunVal"
     }
   }
-  Write-Output "choco install $packages -y"
 
+  choco install $packages -y
   refreshenv
 }
