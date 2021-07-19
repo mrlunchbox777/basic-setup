8856333347435b55f1575adbb8c085f202d465a8
Andrew Shoell <mrlunchbox777@gmail.com>
2021-07-18 22:35:39 -0600

    fixing a bash typo from a while ago

--- install/powershell-installs/Install-ChocoPackage.ps1
+++ install/powershell-installs/Install-ChocoPackage.ps1
@@ -1,6 +1,6 @@
 # Install-ChocoPackageBasicSetup
 function Install-ChocoPackageBasicSetup($package) {
-  $checkForRunVariableName="ShouldInstall_$($1 -replace '-','_')"
+  $checkForRunVariableName="ShouldInstall_$($package -replace '-','_')"
   $checkForRunVariableName="ShouldInstall_$($checkForRunVariableName -replace '.',':')"
   if ($true -eq [System.Environment]::GetEnvironmentVariable("$checkForRunVariableName")) {
     Write-Output "choco install $package -y"
@@ -19,7 +19,7 @@ function Install-ManyChocoPackageBasicSetup() {
   $packages=""
   foreach ($package in $packagesArray) {
     Write-Output "current package - $package"
-    $checkForRunVariableName="ShouldInstall_$($1 -replace '-','_')"
+    $checkForRunVariableName="ShouldInstall_$($package -replace '-','_')"
     if ($true -eq [System.Environment]::GetEnvironmentVariable("$checkForRunVariableName")) {
       Write-Output "adding package - $package"
       $packages+="$package "
