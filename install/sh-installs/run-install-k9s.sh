c16ff875043ae368668b9ea9a543ce447a402dba
Andrew Shoell <mrlunchbox777@gmail.com>
2021-07-21 22:10:33 -0600

    fixing the k9s install

--- install/sh-installs/run-install-k9s.sh
+++ install/sh-installs/run-install-k9s.sh
@@ -3,13 +3,13 @@ run-install-k9s-basic-setup () {
   if [ -z $(which k9s) ]; then
     # https://github.com/derailed/k9s
     local target_dir="${dir}/../../k9s"
-    if [ ! -f "$target_dir" ]; then
+    if [ ! -d "$target_dir" ]; then
       git clone "git@github.com:derailed/k9s.git" "$target_dir"
       local before_k9s_build_dir="$(pwd)"
       cd "$target_dir"
       make build
       cd "$before_k9s_build_dir"
     fi
-    ln -s "${target_dir}/execs/k9s" "/usr/bin/k9s"
+    sudo ln -s "${target_dir}/execs/k9s" "/usr/bin/k9s"
   fi
 }
