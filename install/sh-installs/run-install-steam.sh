1a09a844d5c033798819c42fff995205c7bf7a4a
Andrew Shoell <mrlunchbox777@gmail.com>
2021-07-26 21:47:19 -0600

    adding steam to the installs

---
+++ install/sh-installs/run-install-steam.sh
@@ -0,0 +1,8 @@
+# run install steam function
+run-install-steam-basic-setup () {
+  if [ -z $(which steam) ]; then
+    sudo add-apt-repository multiverse
+    sudo apt-get update
+    sudo apt-get install -y steam
+  fi
+}
