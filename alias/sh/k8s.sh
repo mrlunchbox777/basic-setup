290b628393c9befb945f6b4e9a9309711454eba2
Andrew Shoell <mrlunchbox777@gmail.com>
2021-07-28 22:16:37 -0600

    updating the todo and adding some k8s aliases

---
+++ alias/sh/k8s.sh
@@ -0,0 +1,14 @@
+# adapted from https://betterprogramming.pub/useful-kubectl-aliases-that-will-speed-up-your-coding-54960185d10
+# TODO finish k8s aliases
+alias k=kubectl
+
+alias kg="k get"
+alias kgp="kg pods"
+
+alias kd="k describe"
+alias kdp="kd pod"
+
+alias kc="k config"
+alias kcc="kc current-context"
+alias kcsc="kc set-context"
+alias kcv="kc config view"
