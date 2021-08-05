d4279ec450d5d5e15eaea796b3d748923168cb4e
Andrew Shoell <mrlunchbox777@gmail.com>
2021-08-04 18:59:30 -0600

    adding some todos for network

--- alias/sh/network.sh
+++ alias/sh/network.sh
@@ -10,11 +10,13 @@ my-mac() {
   my-default-network-device | xargs -I{} cat /sys/class/net/{}/address | sed -E ':a;N;$!ba;s/\r{0,1}\n/,/g'
 }
 
+# TODO: handle multiple default devices (wifi & eth)
 my-default-route() {
   local device=$(my-default-network-device)
   ip addr show | awk "/$device/ {print}"
 }
 
+# TODO: handle multiple default devices (wifi & eth)
 my-local-ip() {
   local device=$(my-default-network-device)
   ip addr show | awk "/$device/ {print}" | tr " " "\n" | awk '/([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)|(([a-f0-9:]+:+)+[a-f0-9]+)/ {print;exit}'
