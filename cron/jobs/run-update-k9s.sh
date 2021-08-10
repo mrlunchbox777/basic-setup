d8870d27e98e544abc2cec91f30c471523bd6bd9
Andrew Shoell <mrlunchbox777@gmail.com>
2021-08-09 20:20:35 -0600

    adding k9s to the regular update schedule

---
+++ cron/jobs/run-update-k9s.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+[ ! -d "$shared_scripts_path" ] && shared_scripts_path="./shared-scripts"
+[ ! -d "$shared_scripts_path" ] && shared_scripts_path=$(find /home/ -type d -wholename "*basic-setup/shared-scripts")
+[ ! -d "$shared_scripts_path" ] && shared_scripts_path=$(find $HOME/src/tools -type d -wholename "*basic-setup/shared-scripts")
+[ ! -d "$shared_scripts_path" ] && shared_scripts_path=$(find ./ -type d -wholename "*basic-setup/shared-scripts")
+[ ! -d "$shared_scripts_path" ] && shared_scripts_path=$(find $HOME/src -type d -wholename "*basic-setup/shared-scripts")
+[ ! -d "$shared_scripts_path" ] && shared_scripts_path=$(find $HOME/ -type d -wholename "*basic-setup/shared-scripts")
+[ ! -d "$shared_scripts_path" ] && shared_scripts_path=$(find / -type d -wholename "*basic-setup/shared-scripts")
+if [ ! -d "$shared_scripts_path" ]; then
+    echo -e "error finding shared-scripts..." >&2
+    exit 1
+fi
+for basic_setup_generalrc_sh_f in $(ls -p "$shared_scripts_path/sh/" | grep -v /); do
+  . "$shared_scripts_path/sh/$basic_setup_generalrc_sh_f"
+done
+
+source="${BASH_SOURCE[0]}"
+run-get-source-and-dir "$source"
+source="${rgsd[@]:0:1}"
+dir="${rgsd[@]:1:1}"
+orig_dir="$(pwd)"
+
+cd "$dir/../../install"
+dir="$dir/.."
+BASICSETUPSHOULDFORCEUPDATEK9S="true"
+. ./sh-installs/run-install-k9s.sh
+run-install-k9s-basic-setup
+cd "$orig_dir"
--- cron/run-add-cron.sh
+++ cron/run-add-cron.sh
@@ -30,16 +30,19 @@ cd "$dir"
 should_install_run_update_basic_setup=${BASICSETUPCRONSHOULDINSTALLRUNUPDATEBASICSETUP:-true}
 should_install_run_update_cron_basic_setup=${BASICSETUPCRONSHOULDINSTALLRUNUPDATECRONBASICSETUP:-true}
 should_install_run_update_minikube=${BASICSETUPCRONSHOULDINSTALLRUNUPDATEMINIKUBE:-true}
+should_install_run_update_k9s=${BASICSETUPCRONSHOULDINSTALLRUNUPDATEK9S:-true}
 should_install_run_write_temp_file=${BASICSETUPCRONSHOULDINSTALLRUNWRITETEMPFILE:-false}
 
 # Minute
 run_update_basic_setup_min=${BASICSETUPCRONRUNUPDATEBASICSETUPMIN:-0}
 run_update_cron_basic_setup_min=${BASICSETUPCRONRUNUPDATECRONBASICSETUPMIN:-5}
+run_update_k9s_min=${BASICSETUPCRONRUNUPDATEK9SMIN:-15}
 run_update_minikube_min=${BASICSETUPCRONRUNUPDATEMINIKUBEMIN:-10}
 
 # Hour
 run_update_basic_setup_hour=${BASICSETUPCRONRUNUPDATEBASICSETUPHOUR:-0}
 run_update_cron_basic_setup_hour=${BASICSETUPCRONRUNUPDATECRONBASICSETUPHOUR:-0}
+run_update_k9s_hour=${BASICSETUPCRONRUNUPDATEK9SHOUR:-0}
 run_update_minikube_hour=${BASICSETUPCRONRUNUPDATEMINIKUBEHOUR:-0}
 
 # Day of Month (dom)
@@ -50,6 +53,7 @@ run_update_minikube_hour=${BASICSETUPCRONRUNUPDATEMINIKUBEHOUR:-0}
 
 # Day of Week (dow)
 run_update_minikube_dow=${BASICSETUPCRONRUNUPDATEMINIKUBEDOW:-0}
+run_update_k9s_dow=${BASICSETUPCRONRUNUPDATEK9SDOW:-0}
 
 run-add-cron-basic-setup() {
   if [ -z "$1" ]; then
@@ -97,6 +101,7 @@ run-add-cron-basic-setup() {
 
 run-add-cron-basic-setup "run-update-basic-setup"
 run-add-cron-basic-setup "run-update-cron-basic-setup"
+run-add-cron-basic-setup "run-update-k9s"
 run-add-cron-basic-setup "run-update-minikube"
 run-add-cron-basic-setup "run-write-temp-file"
 
