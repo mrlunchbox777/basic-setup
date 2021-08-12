4de5e76314e8701ab87a1ff5587a99a0ed50672a
Andrew Shoell <mrlunchbox777@gmail.com>
2021-08-11 18:54:07 -0600

    fixing the default route issue

--- alias/sh/network.sh
+++ alias/sh/network.sh
@@ -10,14 +10,12 @@ my-mac() {
   my-default-network-device | xargs -I{} cat /sys/class/net/{}/address | sed -E ':a;N;$!ba;s/\r{0,1}\n/,/g'
 }
 
-# TODO: handle multiple default devices (wifi & eth)
 my-default-route() {
   local device=$(my-default-network-device)
-  ip addr show | awk "/$device/ {print}"
+  echo "$device" | xargs -I % sh -c "ip addr show | awk \"/%/ {print}\""
 }
 
-# TODO: handle multiple default devices (wifi & eth)
 my-local-ip() {
   local device=$(my-default-network-device)
-  ip addr show | awk "/$device/ {print}" | tr " " "\n" | awk '/([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)|(([a-f0-9:]+:+)+[a-f0-9]+)/ {print;exit}'
+  echo "$device" | xargs -I % sh -c "ip addr show | awk \"/%/ {print}\" | tr \" \" \"\\\n\" | awk '/([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)|(([a-f0-9:]+:+)+[a-f0-9]+)/ {print;exit}'"
 }
