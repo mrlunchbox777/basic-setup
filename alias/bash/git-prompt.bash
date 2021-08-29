1e817a745fd2657e3c851bfb9ffde9c5b182cf6d
Andrew Shoell <mrlunchbox777@gmail.com>
2021-08-29 14:21:33 -0600

    got poshgit working

--- alias/bash/git-prompt.bash
+++ alias/bash/git-prompt.bash
@@ -1,3 +1,3 @@
 # Add posh git for bash
-source ../../posh-git-sh/git-prompt.sh
+source $BASICSETUPGENERALRCDIR/../posh-git-sh/git-prompt.sh
 PROMPT_COMMAND='__posh_git_ps1 "\u@\h:\w " "\\\$ ";'$PROMPT_COMMAND
