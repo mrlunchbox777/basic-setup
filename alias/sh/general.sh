ecd4062c3e6152d031e388386b657b0a77e054fa
Andrew Shoell <mrlunchbox777@gmail.com>
2023-07-14 18:03:44 -0600

    moving some of the sh stuff over, and dropping how, which does everything we need

--- alias/sh/general.sh
+++ alias/sh/general.sh
@@ -3,12 +3,12 @@ alias ll="ls -la"
 alias tf="terraform"
 
 alias cddev='general-cddev'
+alias count-lines-dir='general-count-lines-dir'
 alias dfind='general-dfind'
 alias diff-date='general-diff-date'
 alias ffind='general-ffind'
 alias grepr='grep -r'
 alias grepx='general-grepx'
-alias how='general-how'
 alias random='general-random'
 alias read-script='general-read-script'
 alias iso-date='general-iso-date'
--- shared-scripts/sh/run-find-lines-dir.sh
+++
@@ -1,4 +0,0 @@
-# run find lines dir function
-run-find-lines-dir-basic-setup() {
-  find -type f | xargs cat | wc -l
-}
