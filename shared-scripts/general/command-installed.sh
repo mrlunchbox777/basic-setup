95be4e1efa5e655ceeb4e4759eb8f375f656f2ff
Andrew Shoell <mrlunchbox777@gmail.com>
2023-08-15 11:59:36 -0600

    adding nvm

--- shared-scripts/general/command-installed.sh
+++ shared-scripts/general/command-installed.sh
@@ -2,4 +2,4 @@
 
 # TODO: make this more fully fledged
 
-(("$(command -v "$@" >/dev/null 2>&1; echo $?)" == 0)) && echo true || echo false
+(($(command -v "$@" >/dev/null 2>&1; echo $?) == 0)) && echo true || echo false
