a225aa488abc42392f2e9a9f89c72baa97a3266e
Andrew Shoell <mrlunchbox777@gmail.com>
2023-08-22 20:30:29 -0600

    more fixes for gitconfig

--- shared-scripts/git/add-basic-setup-gitconfig.sh
+++ shared-scripts/git/add-basic-setup-gitconfig.sh
@@ -3,6 +3,7 @@ if [ ! -f "$HOME/.gitconfig" ]; then
 fi
 
 if [ -z "$(grep 'path = .*basic-setup-gitconfig' ~/.gitconfig)" ]; then
+	dir="$(general-get-basic-setup-dir)"
 	target_dir=$(readlink -f "$dir/../basic-setup.gitconfig")
 	echo -e "\n[include]\n  path = \"$target_dir\"" >> ~/.gitconfig
 else
