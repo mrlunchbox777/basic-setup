6875d7573c312bb5000548694c54343a07b024d4
Andrew Shoell <mrlunchbox777@gmail.com>
2023-11-14 20:16:38 -0700

    updating docker with basic help info

--- shared-scripts/docker/remove-containers.sh
+++ shared-scripts/docker/remove-containers.sh
@@ -1,4 +1,91 @@
 #! /usr/bin/env bash
 
+#
+# Environment Validation
+#
+validation="$(environment-validation -c -l "core" 2>&1)"
+if [ ! -z "$validation" ]; then
+	echo "Validation error:" >&2
+	echo "$validation" >&2
+	exit 1
+fi
+
+#
+# global defaults
+#
+SHOW_HELP=false
+VERBOSITY=${BASIC_SETUP_VERBOSITY:--1}
+
+#
+# load environment variables
+#
+. basic-setup-set-env
+
+#
+# computed values (often can't be alphabetical)
+#
+if (( $VERBOSITY == -1 )); then
+	VERBOSITY=${BASIC_SETUP_VERBOSITY:-0}
+fi
+
+#
+# helper functions
+#
+
+# script help message
+function help {
+	command_for_help="$(basename "$0")"
+	cat <<- EOF
+		----------
+		usage: $command_for_help <arguments>
+		----------
+		description: remove all local docker containers
+		----------
+		-h|--help    - (flag, current: $SHOW_HELP) Print this help message and exit.
+		-v|--verbose - (multi-flag, current: $VERBOSITY) Increase the verbosity by 1, also set with \`BASIC_SETUP_VERBOSITY\`.
+		----------
+		note: Adapted from and for more info see - https://repo1.dso.mil/big-bang/product/packages/gluon/-/blob/master/docs/bb-package-readme.md
+		----------
+		examples:
+		remove all docker containers - $command_for_help
+		----------
+	EOF
+}
+
+#
+# CLI parsing
+#
+PARAMS=""
+while (("$#")); do
+	case "$1" in
+	# help flag
+	-h | --help)
+		SHOW_HELP=true
+		shift
+		;;
+	# verbosity multi-flag
+	-v | --verbose)
+		((VERBOSITY+=1))
+		shift
+		;;
+	# unsupported flags and arguments
+	-* | --*=)
+		echo "Error: Unsupported flag $1" >&2
+		help
+		exit 1
+		;;
+	# preserve positional arguments
+	*)
+		PARAMS="$PARAMS $1"
+		shift
+		;;
+	esac
+done
+
+#
+# Do the work
+#
+[ $SHOW_HELP == true ] && help && exit 0
+
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
