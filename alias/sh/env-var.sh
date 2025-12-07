51907da606aaba4300313c8fd361444c040ed0bd
Andrew Shoell <mrlunchbox777@gmail.com>
2025-12-07 14:48:57 -0700

    fix: removed pbcopy and pbpaste as aliases for xclip. When needed those will just have to be handled per os or other aliases can be created to support them

--- alias/sh/env-var.sh
+++ alias/sh/env-var.sh
@@ -1,1 +1,25 @@
 #export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
+
+# change gems home to a local directory, but allow this to be overridden
+# by an existing environment variable
+if [ -z "$GEM_HOME" ]; then
+	export GEM_HOME="$HOME/.gems"
+	export PATH="$GEM_HOME/bin:$PATH"
+fi
+
+# if ruby is installed via brew, add its location to the PATH
+if [ -d "/opt/homebrew/opt/ruby/bin" ]; then
+	export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
+fi
+
+# if openjdk is installed via brew, set JAVA_HOME and related variables
+if [ -d "/opt/homebrew/opt/openjdk/libexec/openjdk.jdk" ]; then
+	export JAVA_HOME="/opt/homebrew/opt/openjdk/libexec/openjdk.jdk/Contents/Home"
+	# create a symlink for macOS to find the JDK if it's not already there
+	if [ ! -d "/Library/Java/JavaVirtualMachines/openjdk.jdk" ]; then
+		sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+	fi
+	# add openjdk to PATH and set CPPFLAGS
+	export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
+	export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
+fi
--- shared-scripts/alias/general/pbpaste.sh
+++
@@ -1,5 +0,0 @@
-if [[ "$(environment-os-type)" == "Mac" ]]; then
-	/usr/bin/pbpaste "$@"
-	exit $?
-fi
-xclip -selection clipboard -o "$@"
